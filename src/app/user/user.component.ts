42e4efd9735aa237f352ab1119a8987548fd858d
Omar Badawy <omarbadawy10022@gmail.com>
2024-06-28 18:04:06 +0300

    changed user component to use input signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,18 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
 
-  get imagePath() {
-    return '/assets/users/' + this.avatar;
-  }
+  // input signals NOTE: there are read only signals, can't change their value in this component by calling set method
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  // get imagePath() {
+  //   return '/assets/users/' + this.avatar;
+  // }
+
+  imagePath = computed(() => '/assets/users/' + this.avatar());
 
   onSelectUser() {}
 }
