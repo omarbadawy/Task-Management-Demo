7f33dc797420074ed9b6b5942da5b15f6a207008
Omar Badawy <omarbadawy10022@gmail.com>
2024-06-28 18:05:09 +0300

    returned with old school Input decorators

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,18 +8,18 @@ import { Component, Input, computed, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   // input signals NOTE: there are read only signals, can't change their value in this component by calling set method
-  avatar = input.required<string>();
-  name = input.required<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  // get imagePath() {
-  //   return '/assets/users/' + this.avatar;
-  // }
+  get imagePath() {
+    return '/assets/users/' + this.avatar;
+  }
 
-  imagePath = computed(() => '/assets/users/' + this.avatar());
+  // imagePath = computed(() => '/assets/users/' + this.avatar());
 
   onSelectUser() {}
 }
