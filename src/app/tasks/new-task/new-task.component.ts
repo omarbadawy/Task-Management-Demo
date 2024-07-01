4f6a29f733bc9c861e51c61a974fca6b0c8f1131
Omar Badawy <omarbadawy10022@gmail.com>
2024-07-01 18:42:58 +0300

    Added new-task cancel behavior

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
   imports: [],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  @Output() cancel = new EventEmitter<void>();
 
+  enteredTitle = '';
+
+  onCancel() {
+    this.cancel.emit();
+  }
 }
